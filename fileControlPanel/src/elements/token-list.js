dc884b17f82d006ea41517e884c64a18a670e1db
Roman <mail@romanh.de>
2021-03-31 20:32:22 +0200

    stop fetching tokens all the time

--- fileControlPanel/src/elements/token-list.js
+++ fileControlPanel/src/elements/token-list.js
@@ -45,6 +45,7 @@ export function TokenList(props) {
                 setTokens(res.tokens);
             } else {
                 pushAlert(res, "Error fetching tokens");
+                setTokens([]);
             }
         });
     }
